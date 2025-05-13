488f977bb63d84897446633fb84a28caf7cff4c3
Sanush <sanushgowda7@gmail.com>
2025-05-13 21:42:05 +0530

    Initial commit.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Hardcoded blog posts
+posts = [
+    {'title': 'Welcome to My Blog', 'content': 'This is my first blog post!'},
+    {'title': 'Another Post', 'content': 'Here is another interesting article.'},
+    {'title': 'Flask is Fun', 'content': 'Flask makes web development simple and quick.'}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run(port=8080 , debug=True)
